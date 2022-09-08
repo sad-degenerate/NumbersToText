43d1b9821dfbecdc10df722f7b86b70de3fb0453
Maxim Babakov <mister.franz02@gmail.com>
2022-09-08 10:06:01 +0700

    Класс NumberRange вынесен в отдельный файл.

---
+++ NumbersToText.LIB/NumberRange.cs
@@ -0,0 +1,15 @@
+﻿namespace NumbersToText.LIB
+{
+    public class NumberRange
+    {
+        private readonly int _start;
+        private readonly int _end;
+
+        public NumberRange(int start, int end) => (_start, _end) = (start, end);
+
+        public bool IsInRange(int number)
+        {
+            return number >= _start && number <= _end;
+        }
+    }
+}
--- NumbersToText.LIB/NumbersToTextArrays.cs
+++ NumbersToText.LIB/NumbersToTextArrays.cs
@@ -75,17 +75,4 @@
             { new NumberRange(5, 999), "миллиардов" },
         };
     }
-
-    public class NumberRange
-    {
-        private readonly int _start;
-        private readonly int _end;
-
-        public NumberRange(int start, int end) => (_start, _end) = (start, end);
-
-        public bool IsInRange(int number)
-        {
-            return number >= _start && number <= _end;
-        }
-    }
 }
